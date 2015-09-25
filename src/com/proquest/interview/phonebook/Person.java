73351dc97f9fe4828fe9a901487c62af9ac02444
John Hutchison <jhutchison@gmail.com>
2015-09-25 09:13:49 -0400

    implemented required methods for phoneBookImpl.java

--- src/com/proquest/interview/phonebook/Person.java
+++ src/com/proquest/interview/phonebook/Person.java
@@ -4,4 +4,39 @@ public class Person {
 	public String name;
 	public String phoneNumber;
 	public String address;
+	
+	public Person(String name, String phoneNumber, String address ){
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+
+	public String toString(){
+		return (this.name + "; " +this .phoneNumber +
+				"; " + this.address);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
--- src/com/proquest/interview/phonebook/PhoneBookImpl.java
+++ src/com/proquest/interview/phonebook/PhoneBookImpl.java
@@ -1,31 +1,109 @@
 package com.proquest.interview.phonebook;
 
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import com.proquest.interview.util.DatabaseUtil;
 
 public class PhoneBookImpl implements PhoneBook {
-	public List people;
+	
+	public List <Person> people;
+	private Connection cn;
+	
+	
+	public PhoneBookImpl(){
+		this.people = new ArrayList<Person>();
+		this.cn = null;
+	}
+	
+	private void getConnectionToDb(){
+		if (cn==null){
+			try{
+				cn = DatabaseUtil.getConnection();
+			}catch (Exception e)
+			{
+				System.out.println(e.getMessage());
+			}
+		}
+		return;
+	}
 	
 	@Override
 	public void addPerson(Person newPerson) {
-		//TODO: write this method
+		this.people.add( newPerson );
+		System.out.println(newPerson.toString() + " added to local phonebook");
+	}
+	
+	public String persistPerson(Person person){
+		getConnectionToDb();
+		
+		String name =person.getName();
+		
+		try{
+			Statement stmt = this.cn.createStatement();
+			stmt.execute("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('" 
+				+ name + "', '"
+				+ person.getPhoneNumber() + "', '"
+				+ person.getAddress() + "')"
+				);
+			
+			System.out.println("Sucessfully Persited Person: " + person.toString());
+		
+		}catch (Exception e){
+			System.out.println("ERROR PERSISTING PERSON: " + e.getMessage());
+		}
+		return null;
 	}
 	
 	@Override
-	public Person findPerson() {
-		//TODO: write this method
+	public Person findPerson( String first, String last) {
+		String fullName = first + " " + last;
+		for (Person p : people){
+			if (p.getName().equals(fullName)){
+				return p;
+			}
+		}
+		return null;
 	}
+	
+	public void printAllPeople(){
+		System.out.println("Contents of Local Phonebook:");
+		for (Person p :people){
+			System.out.println(p.toString());
+		}
+	} 
 	
 	public static void main(String[] args) {
 		DatabaseUtil.initDB();  //You should not remove this line, it creates the in-memory database
-
-		/* TODO: create person objects and put them in the PhoneBook and database
+		
+		PhoneBookImpl phoneBook = new PhoneBookImpl();
+		
+		/*create person objects and put them in the PhoneBook and database
 		 * John Smith, (248) 123-4567, 1234 Sand Hill Dr, Royal Oak, MI
 		 * Cynthia Smith, (824) 128-8758, 875 Main St, Ann Arbor, MI
 		*/ 
-		// TODO: print the phone book out to System.out
-		// TODO: find Cynthia Smith and print out just her entry
-		// TODO: insert the new person objects into the database
+		Person person1 = new Person("John Smith", 
+				"(248) 123-4567", "1234 Sand Hill Dr, Royal Oak, MI");
+		Person person2 = new Person("Cynthia Smith",
+				"(824) 128-8758", "875 Main St, Ann Arbor, MI");
+
+		phoneBook.addPerson(person1);
+		phoneBook.persistPerson(person1);
+		phoneBook.addPerson(person2);
+		phoneBook.persistPerson(person2);
+		
+		//print the phone book out to System.out
+		phoneBook.printAllPeople();
+		
+		//find Cynthia Smith and print out just her entry
+		Person cynthiaSmith = phoneBook.findPerson("Cynthia", "Smith");
+		System.out.println( "Successfully Found: " + cynthiaSmith.toString() );
+		
+
+		//TODO insert the new person objects into the database
+		//-- this happens in step 1?--    
 	}
 }
